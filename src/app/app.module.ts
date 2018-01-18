059a65f1b767b1a66e8f4aacc9410f976a2416a1
mgraf1 <mgraf@knox.edu>
2018-01-17 22:49:05 -0600

    added bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { AlertModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { IssueListComponent } from './issue-list/issue-list.component';
@@ -14,7 +15,8 @@ import { IssueService } from './issue.service';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AlertModule.forRoot()
   ],
   providers: [IssueService],
   bootstrap: [AppComponent]
